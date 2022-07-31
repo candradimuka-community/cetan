6e5155de514b6670d416646fc17eaf0359f55fac
aronei44 <meliodasmeliodas224@gmail.com>
2022-07-31 18:43:02 +0700

    feat : swagger open api documentation

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -7,6 +7,59 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      x={
+ *          "logo": {
+ *              "url": "https://via.placeholder.com/190x90.png?text=L5-Swagger"
+ *          }
+ *      },
+ *      title="L5 OpenApi",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *          email="darius@matulionis.lt"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     in="header",
+ *     scheme="bearer",
+ *     securityScheme="bearerAuth",
+ *     name="Authorization"
+ * )
+ * @OA\Get(
+ *    path="/",
+ *    description="Home Page",
+ *    @OA\Response(
+ *        response="default",
+ *        description="Welcome Page"
+ *    )
+ * )
+ * */
+//  * @OA\Get(
+//  *    path="/api/user",
+//  *    description="Get User Information",
+//  *    security={{"bearerAuth":{}}},
+//  *    @OA\Response(
+//  *        response="200",
+//  *        description="User Information"
+//  *    ),
+//  *    @OA\Response(
+//  *        response="401",
+//  *        description="User Unregistered"
+//  *    ),
+//  *    @OA\Response(
+//  *        response="500",
+//  *        description="Server Error"
+//  *    )
+//  * )
+//  */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
