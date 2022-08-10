fff3c8feed03ea77b4ef5c63ccb708ea23b5722f
aronei44 <meliodasmeliodas224@gmail.com>
2022-08-10 07:49:13 +0700

    feat : spinner when load page

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import {UserProvider} from '../context/useUserContext'
 import Head from 'next/head'
+import Spinner from '../components/spinner'
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
                 <title>Cetan Apps</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
+            <Spinner/>
             <Component {...pageProps} />
         </UserProvider>
     )
